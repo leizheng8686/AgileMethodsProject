ef3b1a40696c4d5c9632ab44f6b31ed00c60b728
EXIT <exitudio@gmail.com>
2016-09-15 22:14:40 -0400

    Step 5

--- src/readGEDCOM.java
+++ src/readGEDCOM.java
@@ -6,10 +6,14 @@ public class readGEDCOM {
 	// read a GEDCOM file and output as a .txt
 	public void read(File proj1) {
 		File proj3 = new File("P03.txt");
+		File indi = new File("P03INDI.txt");
+		File fam = new File("P03FAM.txt");
 		BufferedReader reader = null;
 		BufferedReader reader1 = null;
+		BufferedReader reader2 = null;
 		BufferedWriter writer = null;
 		BufferedWriter writer1 = null;
+		BufferedWriter writer2 = null;
 		List<String> list0 = new ArrayList<>();
 		list0.add("INDI");
 		list0.add("FAM");
@@ -33,6 +37,8 @@ public class readGEDCOM {
 		try {
 			reader = new BufferedReader(new FileReader(proj1));
 			writer = new BufferedWriter(new FileWriter(proj3));
+			writer1 = new BufferedWriter(new FileWriter(indi));
+			writer2 = new BufferedWriter(new FileWriter(fam));
 			String str = null;
 			System.out.println("it works");
 			while ((str = reader.readLine()) != null) {
@@ -42,11 +48,18 @@ public class readGEDCOM {
 						if (list0.contains(elems[i])) {
 							writer.write(str + "\r\n");
 						}
+						if (elems[i].equals("INDI")) {
+							writer1.write(elems[1] + " ");
+						}
+
 					}
 				}
 				if (elems[0].equals("1")) {
 					if (list1.contains(elems[1])) {
 						writer.write(str + "\r\n");
+					}
+					if (elems[1].equals("NAME")) {
+						writer1.write(elems[2] + "\r\n");
 					}
 				}
 				if (elems[0].equals("2")) {
@@ -57,6 +70,46 @@ public class readGEDCOM {
 			}
 			reader.close();
 			writer.close();
+			writer1.close();
+			reader1 = new BufferedReader(new FileReader(proj1));
+			String str1 = null;
+			while ((str1 = reader1.readLine()) != null) {
+				reader2 = new BufferedReader(new FileReader(indi));
+				String elems1[] = str1.split(" ");
+				if (elems1[0].equals("0")) {
+					for (int i = 1; i < elems1.length; i++) {
+						if (elems1[i].equals("FAM")) {
+							writer2.write(elems1[1] + "\r");
+						}
+					}
+				}
+				if (elems1[1].equals("HUSB")) {
+					String str2 = null;
+					while ((str2 = reader2.readLine()) != null) {
+						String elems2[] = str2.split(" ");
+						if (elems1[2].equals(elems2[0])) {
+							writer2.write(elems2[0] + " ");
+							writer2.write(elems2[1] + "\r");
+							break;
+						}
+					}
+				}
+				if (elems1[1].equals("WIFE")) {
+					String str3 = null;
+					while ((str3 = reader2.readLine()) != null) {
+						String elems3[] = str3.split(" ");
+						if (elems1[2].equals(elems3[0])) {
+							writer2.write(elems3[0] + " ");
+							writer2.write(elems3[1] + "\r");
+							break;
+						}
+					}
+				}
+				reader2.close();
+			}
+			reader1.close();
+			reader2.close();
+			writer2.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
